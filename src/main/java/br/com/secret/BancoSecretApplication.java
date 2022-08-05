5ffb1ff75f401793bc176f090fb9d170c041571a
Ernor Santos <ernosilva@gmail.com>
2022-08-05 00:28:43 -0300

    Criado projeto

---
+++ src/main/java/br/com/secret/BancoSecretApplication.java
@@ -0,0 +1,13 @@
+package br.com.secret;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoSecretApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoSecretApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/secret/BancoSecretApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.secret;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoSecretApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
